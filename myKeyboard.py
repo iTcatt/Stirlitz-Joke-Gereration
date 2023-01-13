b8b18d4fe85665b3897c20fa371c77a1b1787f4f
Alina <108518096+polzzzik@users.noreply.github.com>
2023-01-13 22:01:07 +0300

    Delete myKeyboard.py

--- myKeyboard.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-kb = ReplyKeyboardMarkup(resize_keyboard=True)
-
-help_button = KeyboardButton('/help')
-gen_button = KeyboardButton('/generate')
-
-kb.add(gen_button).add(help_button)
